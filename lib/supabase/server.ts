85e23b7cae1c4c9eecd2e37d9a22b13f7753b4b9
umhanduhh <amandaables@me.com>
2025-11-06 08:34:15 -0800

    test

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,8 +2,8 @@
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
-export function getSupabaseServerClient() {
-  const cookieStore = cookies();
+export async function getSupabaseServerClient() {
+  const cookieStore = await cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
